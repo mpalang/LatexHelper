80e653b2a539ef13ef710b710f51c9cd7bcf9293
Moritz Lang <93913317+mpalang@users.noreply.github.com>
2021-12-15 12:18:34 +0100

    Update endnotetobibtex.py
    Small adjustments

--- endnotetobibtex.py
+++ endnotetobibtex.py
@@ -20,43 +20,39 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ])
 
-
 #%%
- 
 fp='Literatur.txt'
 save_path=fp.replace('txt','bib')
 
 #%% get Literature
-
 def makeLib(file_path):
-    
     with open(file_path, 'r', encoding='utf-8-sig') as f:
         content=f.read()
         
-    #Sonderzeichen
+    #Sonderzeichen bearbeiten:
     content=content.replace(' & ', ' \& ')
     content=content.replace('\u2009','\u0020')
-
+    #Split content in individual entries:
     content=content.split('\n@')
     Lib={}
     for n,v in enumerate(content):  
         if len(v)>5:
-            
             v=v.lstrip('@')
             lines=v.splitlines()
             temp={}
             lines=[i for i in lines if not 'abstract =' in i]
             for i in lines:
-
                 if 'type' in i:
                     temp['type']=re.split('{|}', i)[1]
             try:
-                    temp['type']
+                temp['type']
             except:
-                    logging.critical(f'{n}th element has no type:\n {v}')
-                    sys.exit()
-
+                logging.critical(f'{n}th element has no type:\n {v}')
+                sys.exit()
+            
+            #Create Bibkey for different Types:
             DefaultTypes=['Journal Article','Book','Book Section']
+            
             if temp['type'] in DefaultTypes:
                 for i in lines:
                     if 'author' in i:
@@ -75,11 +71,8 @@ def makeLib(file_path):
             else:
                 logging.critical(f"Literaturetype {temp['type']} not known.")
                 sys.exit()
-            
-            # create Bibkey for .bib and check if same name exists already    
-            
-            
-             #replace special characters
+                        
+            #replace special characters in Bibkey and check if already exists. If yes add second author to Bibkey and so on...
             bibkey=re.sub('é','e',bibkey)
             bibkey=re.sub('á','a',bibkey)
             bibkey=re.sub('ä','a',bibkey)
@@ -101,10 +94,11 @@ def makeLib(file_path):
                 else:
                     break
             
-            #print(bibkey)
+            #And put everything back together. Add new field "ReferenceNumber" with Endnote RN:
             h=lines[0].split('{')
             h[1]=re.sub(',| ','',h[1])
             lines[0]='@'+h[0]+'{'+bibkey+','
+            
             if 'ReferenceNumber' in lines[1]:
                 lines[1]='   '+'ReferenceNumber = ' + '{' + h[1] + '}' + ','
             else:
@@ -114,6 +108,7 @@ def makeLib(file_path):
             if '@@' in contTemp:
                 contTemp.replace('@@','@')
             #contTemp.replace('inbook','incollection')   
+            
             Lib[bibkey]=contTemp
 
     return Lib
